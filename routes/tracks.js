c89bd18ea5d4c04f314fc52d2765ac3f77c16896
Tomas ortega <ortegaxc.05@gmail.com>
2022-03-19 19:03:27 -0300

    Api Nodejs

---
+++ routes/tracks.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router();
+const { getItems, getItem, createItem } = require('../controllers/tracks');
+
+//TODO http:localhost/tracks GET, POST, DELETE, PUT
+
+router.get('/', getItems);
+
+router.post('/', createItem);
+
+module.exports = router
